2781d08387bce2dd737e60524137cf57bfa97303
Asendant <jalthen@calpoly.edu>
2025-09-08 23:58:50 -0700

    Added Heading component that contains gradient title header. Attatched header component to app.js

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,10 @@
-function App() {
+import Heading from "./components/heading/heading";
 
+function App() {
   return (
-    <>
-      
-    </>
+    <div className="bg-neutral-900 w-screen h-screen pt-10">
+      <Heading />
+    </div>
   )
 }
 
---
+++ frontend/src/components/heading/heading.tsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <div className="w-screen flex justify-center">
+        <h1
+        className="font-extrabold text-5xl bg-gradient-to-r from-indigo-400 via-violet-500 via-fuchsia-500 to-indigo-400 bg-clip-text text-transparent inline-block"
+        >
+        What's for Dinner?
+        </h1>
+    </div>
+  );
+}
+
+export default Header;
